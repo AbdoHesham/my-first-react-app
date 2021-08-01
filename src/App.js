047e022293cab5d1ccdba4fb076a3b1ceb37ad8d
MohamedSalah111500 <mohamedsalah111500@gmail.com>
2021-08-01 04:24:14 +0300

    upload 1.8.1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+import Nav_bar from './nav-bar';
+import Home from './home'
+
+function App() {
+    return ( <
+        div >
+        <
+        Nav_bar / >
+
+        <
+        Home / >
+        <
+        /div>
+    );
+}
+
+export default App;
---
+++ src/nav-bar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { NavDropdown, Nav, Navbar, Container } from "react-bootstrap";
+
+class Nav_bar extends Component {
+  state = {
+    count: 0,
+  };
+  render() {
+    const classes=  this.state.count > 0 ? "dark" : "light" 
+
+    return (
+        <Navbar bg={classes} expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto text-center">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    
+       
+      
+    );
+  }
+}
+
+export default Nav_bar;
